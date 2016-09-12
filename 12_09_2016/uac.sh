5a38a71c8311b26c019d1ecd6d0436bf3b50018a
Oleg Golosovskiy <ogolosovskiy@gmail.com>
2016-09-12 12:37:26 +0300

    1000cps 60sec calls

---
+++ 12_09_2016/uac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./sipp -sf ./uac.xml -i 192.168.1.110 -rtp_echo -p 5080 -r 1000 -m 1000000 -trace_err 192.168.1.110:5060
+
+
+
+
---
+++ 12_09_2016/uas.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./sipp -sf ./uas.xml -i 127.0.0.1 -rtp_echo -p 6000 -trace_err 
+
+
